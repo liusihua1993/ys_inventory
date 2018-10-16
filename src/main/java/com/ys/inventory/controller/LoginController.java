aea871cab9469d217fd0b322ce4f659918546d3c
liusihua <1398164936@qq.com>
2018-10-16 09:40:13 +0800

    @RequestBody 接口添加注解

--- src/main/java/com/ys/inventory/controller/LoginController.java
+++ src/main/java/com/ys/inventory/controller/LoginController.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -34,7 +35,7 @@ public class LoginController {
     @ApiResponses({@ApiResponse(code = 200, message = "登录成功", response = Result.class)})
     @LogAction(value = "登录")
     @PostMapping(value = "/login")
-    public User add(@ApiParam(value = "登录信息", required = true) LoginVO vo) {
+    public User add(@ApiParam(value = "登录信息", required = true) @RequestBody LoginVO vo) {
         return loginService.login(vo);
     }
 
